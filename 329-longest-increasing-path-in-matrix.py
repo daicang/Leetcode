2e810b4d19709d456057e54918faa42cc221102c
daicang <daicang.cn@gmail.com>
2016-05-05 09:41:49 +0800

    Diff: hard.  DP traverse.

---
+++ 329-longest-increasing-path-in-matrix.py
@@ -0,0 +1,60 @@
+# 329-longest-increasing-path-in-matrix.py
+
+class Solution(object):
+    def longestIncreasingPath_wa(self, matrix): # Wrong Answer, need 4 directions
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        if len(matrix) == 0: return 0
+        d1 = [[1] * len(matrix[0]) for _ in matrix] # Greater
+        d2 = [[1] * len(matrix[0]) for _ in matrix] # Lesser
+        maxLength = 1
+        
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                if i == 0 and j == 0: continue
+                if i == 0:
+                    if matrix[i][j] > matrix[i][j-1]:
+                        d1[i][j] = d1[i][j-1] + 1
+                    elif matrix[i][j] < matrix[i][j-1]:
+                        d2[i][j] = d2[i][j-1] + 1
+                elif j == 0:
+                    if matrix[i][j] > matrix[i-1][j]:
+                        d1[i][j] = d1[i-1][j] + 1
+                    elif matrix[i][j] < matrix[i-1][j]:
+                        d2[i][j] = d2[i-1][j] + 1
+                else:
+                    if matrix[i][j] > matrix[i-1][j]:
+                        d1[i][j] = d1[i-1][j] + 1
+                    elif matrix[i][j] < matrix[i-1][j]:
+                        d2[i][j] = d2[i-1][j] + 1
+                        
+                    if matrix[i][j] > matrix[i][j-1]:
+                        d1[i][j] = max(d1[i][j], d1[i][j-1]+1)
+                    elif matrix[i][j] < matrix[i][j-1]:
+                        d2[i][j] = max(d2[i][j], d2[i][j-1]+1)
+
+                maxLength = max(maxLength, d1[i][j], d2[i][j])
+
+        return maxLength
+
+    def longestIncreasingPath(self, matrix):
+        def dfs(x, y):
+            if not d[x][y]: # Only calculate once.
+                curr = matrix[x][y]
+                d[x][y] = 1 + max(
+                    dfs(x-1, y) if x > 0 and curr > matrix[x-1][y] else 0,
+                    dfs(x+1, y) if x < lx-1 and curr > matrix[x+1][y] else 0,
+                    dfs(x, y-1) if y > 0 and curr > matrix[x][y-1] else 0,
+                    dfs(x, y+1) if y < ly-1 and curr > matrix[x][y+1] else 0)
+            return d[x][y]
+
+        if not matrix or not matrix[0]: return 0
+        lx, ly = len(matrix), len(matrix[0])
+        d = [[0] * ly for _ in xrange(lx)]
+        return max(dfs(x, y) for x in range(lx) for y in range(ly))
+
+s = Solution()
+print s.longestIncreasingPath([[9,9,4],[6,6,8],[2,1,1]])
+print s.longestIncreasingPath([[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]])
