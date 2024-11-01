cdbb67fb39218ae26ca62ce4a7ace5d2d94730aa
Dai Cang <daicang.cn@gmail.com>
2024-11-01 08:19:57 +0800

    Update

--- 0198-house-robber.py
+++ 0198-house-robber.py
@@ -1,35 +1,7 @@
-from typing import List
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 3:
-            return max(nums)
+        v_rob, v_norob = 0, 0
+        for i, val in enumerate(nums):
+            v_rob, v_norob = val+v_norob, max(v_rob, v_norob)
 
-        # rob(k) = n+norob(k-1)
-        # norob(k-1) = max(norob(k-1), rob(k-1))
-        # rob(0) = nums[0]
-        # norob(0) = 0
-
-        norob_value = 0
-        rob_value = nums[0]
-
-        for i, n in enumerate(nums):
-            if i > 0:
-                new_rob_value = n + norob_value
-                new_norob_value = max(norob_value, rob_value)
-
-                rob_value = new_rob_value
-                norob_value = new_norob_value
-
-        return max(rob_value, norob_value)
-
-
-s = Solution()
-
-inputs = [
-    [1,2,3,1],    # 4
-    [2,7,9,3,1],  # 12
-]
-
-for i in inputs:
-    print(s.rob(i))
+        return max(v_rob, v_norob)
--- 0216-combination-sum-iii.py
+++ 0216-combination-sum-iii.py
@@ -1,41 +1,15 @@
-import copy
-from typing import List
-
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         result = []
 
-        def backtrack(largest, k, remain, comb):
-            if remain == 0:
-                if k == 0:
-                    result.append(copy.copy(comb))
+        def traverse(i, k, nums):
+            if k == 0 and sum(nums) == n:
+                result.append(nums[:])
                 return
+            for j in range(i, 10):
+                nums.append(j)
+                traverse(j+1, k-1, nums)
+                nums.pop()
 
-            if largest == 0:
-                return
-
-            if largest > remain:
-                largest = remain
-
-            # pick largest
-            comb.append(largest)
-            backtrack(largest-1, k-1, remain-largest, comb)
-
-            # skip largest
-            comb.pop()
-            backtrack(largest-1, k, remain, comb)
-
-        backtrack(9, k, n, [])
-
+        traverse(1, k, [])
         return result
-
-s = Solution()
-
-inputs = [
-    [3, 7],
-    [3, 9],
-    [4, 1]
-]
-
-for i in inputs:
-    print(s.combinationSum3(*i))
